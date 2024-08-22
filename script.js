b9b72441cf8d3cd8be7eb3fc10d9d911c0fca9d3
Dat Nguyen <nguy5079@umn.edu>
2024-08-22 18:48:54 -0500

    animate

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
+const mainTitle = document.querySelector(".main-title");
+const menuBut = document.querySelector(".icon");
+const bars = document.querySelectorAll(".bar");
 
-gsap.to(".box", {
-    x: 1000,
+let mainTitleTl = gsap.timeline({paused: true}).to(mainTitle, {
+    x: "-1rem",
+    y: "-1rem",
+    ease: "power4.out",
 });
+
+let menuButTl = gsap.timeline({paused: true}).to(menuBut, {
+    x: "-1rem",
+    y: "-1rem",
+    ease: "power4.out",
+});
+
+menuBut.onpointerenter = () => {
+    menuButTl.play();
+};
+menuBut.onpointerleave = () => {
+    menuButTl.reverse();
+};
+mainTitle.onpointerenter = () => {
+    mainTitleTl.play();
+}
+
+mainTitle.onpointerleave = () => {
+    mainTitleTl.reverse();
+};
+
+menuBut.onclick = () => {
+    gsap.to(bars, {
+        y: "100%",
+        stagger: 0.1,
+    })
+}
